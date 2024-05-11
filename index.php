5cf993ac8cc8767479310efb08ecc525f324c6d2
Huallison Tavares <huallisonleite@gmail.com>
2024-05-11 13:37:17 -0300

    Ajustes do CSS da Página Inicial

--- index.php
+++ index.php
@@ -4,17 +4,18 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Management</title>
-    <link rel="stylesheet" href="./src/style.css">
+    <link rel="stylesheet" href="./src/css/reset.css">
+    <link rel="stylesheet" href="./src/css/style.css">
     <script src="./src/js/app.js"></script>
 </head>
 <body>
     <header>
-        <img src="" alt="Kanban">
+        <img src="./src/img/logo.png" alt="Logo">
         <nav>
             <p>Todos os quadros</p>
 
             <ul>
-                <li>Plataforma de A</li>
+                <li id="select">Plataforma de A</li>
                 <li>Plataforma de B</li>
                 <li>Plataforma de C</li>
                 <li> + Criar novo quadro</li>
@@ -23,65 +24,80 @@
     </header>
 
     <main>
-        <div>
+        <div class="header-task">
             <h1>Plataforma Launch</h1>
             <button>+ Adicionar Nova Tarefa</button>
         </div>
 
-        <div class="tasks-column">
-            <div class="tasks">
-                <h3>Pendencias</h3>
-    
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
+        <div class="tasks-columns">
+            <div class="task-column">
+                <div class="tasks">
+                    <h3>Pendencias</h3>
+        
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
                 </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
+            </div>
+            <div class="task-column">
+                <div class="tasks">
+                    <h3>Pendencias</h3>
+        
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
                 </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
+            </div>
+            <div class="task-column">
+                <div class="tasks">
+                    <h3>Pendencias</h3>
+        
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
+                    <div class="task">
+                        <h2>Criar tela inicial</h2>
+                        <p>0 de 4 completados</p>
+                    </div>
                 </div>
             </div>
         </div>
-        <div class="tasks-column">
-            <div class="tasks">
-                <h3>Pendencias</h3>
-    
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-            </div>
-        </div>
-        <div class="tasks-column">
-            <div class="tasks">
-                <h3>Pendencias</h3>
-    
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-                <div class="task">
-                    <h2>Criar tela inicial</h2>
-                </div>
-            </div>
-        </div>
+        
 
     </main>
 </body>

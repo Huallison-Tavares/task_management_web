0466735cb68a5d0751f803e3aa2fcf7748f6503d
Huallison Tavares <huallisonleite@gmail.com>
2024-05-11 11:29:18 -0300

    Atulizando o index e criando arquivo js

--- index.php
+++ index.php
@@ -4,8 +4,85 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Task Management</title>
+    <link rel="stylesheet" href="./src/style.css">
+    <script src="./src/js/app.js"></script>
 </head>
 <body>
-    <h1>Olá mundo</h1>
+    <header>
+        <img src="" alt="">
+        <nav>
+            <p>Todos os quadros</p>
+
+            <ul>
+                <li>Plataforma de A</li>
+                <li>Plataforma de B</li>
+                <li>Plataforma de C</li>
+                <li> + Criar novo quadro</li>
+            </ul>
+        </nav>
+    </header>
+
+    <main>
+        <div>
+            <h1>Plataforma Launch</h1>
+            <button>+ Adicionar Nova Tarefa</button>
+        </div>
+
+        <div class="tasks-column">
+            <div class="tasks">
+                <h3>Pendencias</h3>
+    
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+            </div>
+        </div>
+        <div class="tasks-column">
+            <div class="tasks">
+                <h3>Pendencias</h3>
+    
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+            </div>
+        </div>
+        <div class="tasks-column">
+            <div class="tasks">
+                <h3>Pendencias</h3>
+    
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+                <div class="task">
+                    <h2>Criar tela inicial</h2>
+                </div>
+            </div>
+        </div>
+
+    </main>
 </body>
 </html>
